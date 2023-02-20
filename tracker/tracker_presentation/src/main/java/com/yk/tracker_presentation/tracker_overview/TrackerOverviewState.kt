0405d6d5033f599033e17f3bd9f540cee4d43449
YosifKalchev <ykalchev@icloud.com>
2023-02-20 22:59:03 +0200

    Added icons for basic meals, added new variable in sharedPrefs, create meal type, states for tracker feature and methods of the view model

---
+++ tracker/tracker_presentation/src/main/java/com/yk/tracker_presentation/tracker_overview/TrackerOverviewState.kt
@@ -0,0 +1,18 @@
+package com.yk.tracker_presentation.tracker_overview
+
+import com.yk.tracker_domain.model.TrackedFood
+import java.time.LocalDate
+
+
+data class TrackerOverviewState(
+    val totalCarbs: Int = 0,
+    val totalProtein: Int = 0,
+    val totalFat: Int = 0,
+    val totalCalories: Int = 0,
+    val carbsGoal: Int = 0,
+    val proteinGoal: Int = 0,
+    val fatGoal: Int = 0,
+    val data: LocalDate = LocalDate.now(),
+    val trackedFood: List<TrackedFood> = emptyList(),
+    val meals: List<Meal> = defaultMeals
+)
